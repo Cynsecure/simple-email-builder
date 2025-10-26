ad42711917f1368f82d88484b96f9e78698960cf
Uddipan Bhattacharyya <uddipan.bhatta7@gmail.com>
2025-10-27 03:05:33 +0530

    build: 0.0.1

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-# @usewaypoint/editor-sample
+# Simple email editor
 
-Use this as a sample to self-host EmailBuilder.js.
-
-To run this locally, fork the repository and then in this directory run:
+# Run
 
 - `npm install`
 - `npx vite`
 
-Once the server is running, open http://localhost:5173/email-builder-js/ in your browser.
+# Build
+
+- `npm run clean`
+- `npm run compile`
+- `npm pack`
+
+# Import
+
+```js
+import Editor from "simple-email-builder";
+// or
+import { EmailEditor } from "simple-email-builder";
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite';
-
 import react from '@vitejs/plugin-react-swc';
+import pkg from './package.json';
+
+const peerDeps = Object.keys(pkg.peerDependencies || {});
+const deps = Object.keys(pkg.dependencies || {});
+const external = [...peerDeps, /* optionally externalize some deps from deps */];
 
 export default defineConfig({
   plugins: [react()],
   base: '/email-builder-js/',
+  build: {
+    lib: {
+      entry: 'src/Editor/index.tsx',
+      name: 'SimpleEmailBuilder', // global UMD name
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      external,
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          // add other globals as required for UMD
+        },
+      },
+    },
+  },
 });
