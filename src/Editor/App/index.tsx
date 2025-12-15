73efc35db6e4ec2f61d5652aa9ad68dda11cb8cc
Kamal <kamalbagchi9@gmail.com>
2025-12-15 14:14:42 +0530

    Minor change

--- src/Editor/App/index.tsx
+++ src/Editor/App/index.tsx
@@ -403,6 +403,20 @@ export default function Editor({
   useEffect(() => {
     if (template) {
       resetDocument(template);
+    } else {
+      // Reset when no template is provided
+      resetDocument({
+        root: {
+          type: "EmailLayout",
+          data: {
+            backdropColor: "#F5F5F5",
+            canvasColor: "#FFFFFF",
+            textColor: "#262626",
+            fontFamily: "MODERN_SANS",
+            childrenIds: [],
+          },
+        },
+      });
     }
   }, [template]);
 
